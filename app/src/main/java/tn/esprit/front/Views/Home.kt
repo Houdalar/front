0e77f1952fd82a4f0601b066fba2a3ea9d193bfa
Said-Ines <inessaid1905@gmail.com>
2022-11-16 20:09:15 +0100

    commit

--- app/src/main/java/tn/esprit/front/Views/Home.kt
+++ app/src/main/java/tn/esprit/front/Views/Home.kt
@@ -2,14 +2,89 @@ package tn.esprit.front.Views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import com.example.front.*
+import kotlinx.android.synthetic.main.home.*
 import tn.esprit.front.R
-class Home : AppCompatActivity()
-{
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
-    }
-}
-//activate the user account by clicking on the link sent to his email address
 
+        nav_view.setNavigationItemSelectedListener {
+            it.isChecked=true
+
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    replaceFragment(HomeFragment(),it.title.toString())
+                }
+                R.id.nav_babies -> {
+                    replaceFragment(BabiesFragment(),it.title.toString())
+                }
+                R.id.nav_music -> {
+                    replaceFragment(MusicFragment(),it.title.toString())
+                }
+                R.id.nav_Audiobooks -> {
+                    replaceFragment(AudiobooksFragment(),it.title.toString())
+                }
+                R.id.nav_settings -> {
+                    replaceFragment(SettingsFragment(),it.title.toString())
+                }
+                R.id.nav_logout -> {
+                    true
+                }
+                R.id.nav_help -> {
+                    replaceFragment(HelpFragment(),it.title.toString())
+                }
+            }
+            true
+        }
+
+        val toggle = ActionBarDrawerToggle(this, DrawerLayout, R.string.open, R.string.close)
+        DrawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun replaceFragment(fragment: Fragment,title: String){
+        val fragmentManager=supportFragmentManager
+        val fragmentTransaction=fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.FrameLayout,fragment)
+        fragmentTransaction.commit()
+        DrawerLayout.closeDrawers()
+        setTitle(title)
+
+
+    }
+
+
+    override fun onBackPressed(){
+        when {
+            //If drawer layout is open close that on back pressed
+            DrawerLayout.isDrawerOpen(GravityCompat.START)->{
+                DrawerLayout.closeDrawer(GravityCompat.START)
+            }
+            else->{
+                //If drawer is already in closed condition then go back
+                super.onBackPressed()
+            }}
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        return when(item.itemId){
+            android.R.id.home ->{
+                DrawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+}
