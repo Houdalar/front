9fc35fb5a2ca3bb3f291fae2701c3e6194369f0c
Said-Ines <inessaid1905@gmail.com>
2022-12-07 11:08:03 +0100

    commit

---
+++ app/src/main/java/tn/esprit/front/models/Baby.kt
@@ -0,0 +1,19 @@
+package tn.esprit.front.models
+
+import com.google.gson.annotations.SerializedName
+
+const val NAME = "NAME"
+const val PICTURE="PICTURE"
+
+data class Baby(
+    @SerializedName("babyName")
+    var babyName:String?=null,
+    @SerializedName("birthday")
+    var birthday: String?=null,
+    @SerializedName("babyPic")
+    var babyPic: String?=null,
+    @SerializedName("parent")
+    var parent:String?=null,
+    var token:String?=null,
+
+    )
